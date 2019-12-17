907bf8f5c1c20ed90e1edf65ad7b4b26099e5b3c
captivea-ssa <56271206+captivea-ssa@users.noreply.github.com>
2019-12-17 10:37:37 -0500

    Reverted the changes made to __init__.py

--- sale_commission/models/__init__.py
+++ sale_commission/models/__init__.py
@@ -4,5 +4,5 @@ from . import sale_commission_mixin
 from . import sale_commission
 from . import res_partner
 from . import sale_order
-from . import account_move
+from . import account_invoice
 from . import settlement
