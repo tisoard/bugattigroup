50be0ad8199345bd2d3801e3c208517d6b3ffa19
tisoard <56162576+tisoard@users.noreply.github.com>
2020-05-06 16:11:00 -0400

    BUG FIX - Sale.py
    Correction of the issue when delivery is created with the main product of the bundle pack, the main product was always deducted by one instead of taking the SO qty.

--- product_bundle_pack/models/sale.py
+++ product_bundle_pack/models/sale.py
@@ -194,18 +194,18 @@ class SaleOrderLine(models.Model):
                     return 0.0
         return order
 
-class ProcurementRule(models.Model):
-    _inherit = 'stock.rule'
-    
-    def _get_stock_move_values(self, product_id, product_qty, product_uom, location_id, name, origin, values, group_id):
-        result = super(ProcurementRule, self)._get_stock_move_values(product_id, product_qty, product_uom, location_id, name, origin, values, group_id)
-        
-        if  product_id.pack_ids:
-            for item in product_id.pack_ids:
-                result.update({
-                    'product_id': item.product_id.id,
-                    'product_uom': item.uom_id and item.uom_id.id,
-                    'product_uom_qty': item.qty_uom,
-                    'origin': origin,
-                    })
-        return result
+#class ProcurementRule(models.Model):
+#    _inherit = 'stock.rule'
+#    
+#    def _get_stock_move_values(self, product_id, product_qty, product_uom, location_id, name, origin, values, group_id):
+#        result = super(ProcurementRule, self)._get_stock_move_values(product_id, product_qty, product_uom, location_id, name, origin, values, group_id)
+#        
+#        if  product_id.pack_ids:
+#            for item in product_id.pack_ids:
+#                result.update({
+#                    'product_id': item.product_id.id,
+#                    'product_uom': item.uom_id and item.uom_id.id,
+#                    'product_uom_qty': item.qty_uom,
+#                    'origin': origin,
+#                    })
+#        return result
